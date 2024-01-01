ca436e8e8f52b7358e006b333ec6187bf908916c
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-01 14:09:00 +1100

    added horizontal checking and created winner setting function

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let gameOver = false;
 let board;
 let rows = 6;
 let columns = 7;
-var currColumns;
+let currColumns;
 
 window.onload = function () {
   setGame();
@@ -21,7 +21,7 @@ function setGame() {
       row.push(" ");
 
       let tile = document.createElement("div");
-      tile.id(r.toString() + "-" + c.toString());
+      tile.id = r.toString() + "-" + c.toString();
       tile.classList.add("tile");
       tile.addEventListener("click", setPiece);
       document.querySelector(".board").appendChild(tile);
@@ -45,9 +45,10 @@ function setPiece() {
   }
 
   board[r][c] = currPlayer;
-  let tile = document.querySelector(r.toString() + "-" + c.toString());
+  let tile = document.getElementById(`${r}-${c}`);
+  console.log(tile);
 
-  if (currPlayer == playerRed) {
+  if (currPlayer === playerRed) {
     tile.classList.add("red-piece");
     currPlayer = playerYellow;
   } else {
@@ -57,4 +58,36 @@ function setPiece() {
 
   r -= 1;
   currColumns[c] = r;
+
+  checkWinner();
+}
+
+function checkWinner() {
+  // horizontal sliding window checking
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+      if (board[r][c] != " ") {
+        if (
+          board[r][c] == board[r][c + 1] &&
+          board[r][c + 1] == board[r][c + 2] &&
+          board[r][c + 2] == board[r][c + 3]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
+}
+
+function setWinner(r, c) {
+  let winner = document.querySelector(".winner");
+
+  if (board[r][c] == playerRed) {
+    winner.innerText = "Red wins!";
+  } else {
+    winner.innerText = "Yellow wins!";
+  }
+
+  gameOver = true;
 }
